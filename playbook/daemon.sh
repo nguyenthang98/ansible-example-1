8d89d057169608aebab958cb3e717147bddd23cd
Nguyen Duc Thang <nguyenducthang.uet@gmail.com>
2020-09-20 22:00:30 +0700

    update

--- playbook/daemon.sh
+++ playbook/daemon.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LOG_FILE=/tmp/ansible-project.log
 function daemon() {
     count=0
     chsum1=""
@@ -7,15 +8,18 @@ function daemon() {
             chsum2=`find ./ -type f \( ! -name "*.swp" \) -exec md5sum {} \;`
             if [[ $chsum1 != $chsum2 ]] ; then           
                 # save latest execution logs
+                START_TIME = $(date +%s)
                 if [[ $count -gt 5 ]]; then
-                  echo "Detect file changed at: $(date)" > /tmp/ansible-project.log
+                  echo "Detect file changed at: $(date)" > $LOG_FILE
                   count=0
                 else
-                  echo "Detect file changed at: $(date)" >> /tmp/ansible-project.log
+                  echo "Detect file changed at: $(date)" >> $LOG_FILE
                 fi
                 ./run.sh >> /tmp/ansible-project.log
                 chsum1=$chsum2
                 ((count++))
+                END_TIME = $(date +%s)
+                echo "Process took $(( $END_TIME - $START_TIME)) seconds to complete..." >> $LOG_FILE
             fi
             sleep 2
         done
